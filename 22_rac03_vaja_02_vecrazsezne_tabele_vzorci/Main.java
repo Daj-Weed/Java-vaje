125a5a7e56f1c8f1bee12a11bd11c6b439ce0246
David Sedej <73789272+Daj-Weed@users.noreply.github.com>
2023-04-24 14:34:10 +0200

    Update Main.java

--- 22_rac03_vaja_02_vecrazsezne_tabele_vzorci/Main.java
+++ 22_rac03_vaja_02_vecrazsezne_tabele_vzorci/Main.java
@@ -15,9 +15,9 @@ public class Main {
 
                   {9,6,1, 5,3,7, 2,8,4},
                   {2,8,7, 4,1,9, 6,3,5},
-                  {3,4,5, 2,6,6, 1,7,9}};
+                  {3,4,5, 2,8,6, 1,7,9}};
 
-    System.out.println(sodokuA.aliJeSudoku3x3kvadrat(tn, 8));
+    System.out.println(sodokuA.aliJeSudokuResen(tn));
   }
 
 }
