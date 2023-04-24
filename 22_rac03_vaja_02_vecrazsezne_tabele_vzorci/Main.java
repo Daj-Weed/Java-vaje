a4f7c308b459331ff5b96f4d762a1e50b0c2e11b
David Sedej <73789272+Daj-Weed@users.noreply.github.com>
2023-04-24 14:14:08 +0200

    Update Main.java

--- 22_rac03_vaja_02_vecrazsezne_tabele_vzorci/Main.java
+++ 22_rac03_vaja_02_vecrazsezne_tabele_vzorci/Main.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
     SodokuA sodokuA = new SodokuA();
 
-    int[][] tn = {{5,3,4, 6,7,9, 9,1,2},
+    int[][] tn = {{5,3,4, 6,7,8, 9,1,2},
                   {6,7,2, 1,9,5, 3,4,8},
                   {1,9,8, 3,4,2, 5,6,7},
 
